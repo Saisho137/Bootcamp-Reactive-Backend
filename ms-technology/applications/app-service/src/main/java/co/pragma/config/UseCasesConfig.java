c755b76963f5db64e8df8cf9e1c8c772a2760c8b
Santiago Betancur Duque <santib.2001@hotmail.com>
2025-02-18 17:33:05 -0500

    feat: add reactive-web module with working HealthCheck Controller

--- ms-technology/applications/app-service/src/main/java/co/pragma/config/UseCasesConfig.java
+++ ms-technology/applications/app-service/src/main/java/co/pragma/config/UseCasesConfig.java
@@ -1,7 +1,11 @@
 package co.pragma.config;
 
 import co.pragma.logic.TechnologyGateway;
+import co.pragma.r2dbc.mapper.TechnologyMapper;
+import co.pragma.r2dbc.repository.TechnologyRepository;
+import co.pragma.r2dbc.service.TechnologyService;
 import co.pragma.usecase.TechnologyUseCase;
+import org.mapstruct.factory.Mappers;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,5 +14,15 @@ public class UseCasesConfig {
     @Bean
     public TechnologyUseCase technologyUseCase(TechnologyGateway technologyGateway) {
         return new TechnologyUseCase(technologyGateway);
+    }
+
+    @Bean
+    public TechnologyMapper technologyMapper() {
+        return Mappers.getMapper(TechnologyMapper.class);
+    }
+
+    @Bean
+    public TechnologyGateway technologyGateway(TechnologyRepository technologyRepository, TechnologyMapper technologyMapper) {
+        return new TechnologyService(technologyRepository, technologyMapper);
     }
 }
---
+++ ms-technology/infrastructure/entry-points/reactive-web/src/main/java/co/pragma/api/config/HealthCheckController.java
@@ -0,0 +1,14 @@
+package co.pragma.api.config;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HealthCheckController {
+    @GetMapping(path = "/validate-service")
+    public String validateServiceOn() {
+        return "The microservice is up";
+    }
+}
