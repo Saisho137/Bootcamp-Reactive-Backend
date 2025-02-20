27ee1bbd612c852f0376de7c4f065c3bb2138bef
Santiago Betancur Duque <santib.2001@hotmail.com>
2025-02-20 09:38:43 -0500

    fix: solved problem with DB queries

--- ms-technology/infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/pragma/r2dbc/dto/TechnologyDTO.java
+++ ms-technology/infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/pragma/r2dbc/dto/TechnologyDTO.java
@@ -1,17 +1,20 @@
 package co.pragma.r2dbc.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
 @Data
 @Builder
-@Table("technologies")
+@AllArgsConstructor
+@NoArgsConstructor
+@Table("ms_technology.technologies")
 public class TechnologyDTO {
     @Id
     private Long id;
-
     private String name;
     private String description;
 }
--- ms-technology/infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/pragma/r2dbc/repository/TechnologyRepository.java
+++ ms-technology/infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/pragma/r2dbc/repository/TechnologyRepository.java
@@ -1,9 +1,11 @@
 package co.pragma.r2dbc.repository;
 
 import co.pragma.r2dbc.dto.TechnologyDTO;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Mono;
 
 public interface TechnologyRepository extends ReactiveCrudRepository<TechnologyDTO, Long> {
+    @Query("SELECT * FROM ms_technology.technologies WHERE name ILIKE :name")
     Mono<TechnologyDTO> findByName(String name);
 }
