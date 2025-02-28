7c70285f256f121b2989f61e70610ff1113bbc26
Santiago Betancur Duque <santib.2001@hotmail.com>
2025-02-28 15:32:37 -0500

    feat: add basic Project schema with Ports & Adapters Architecture

---
+++ ms_bootcamp/src/main/java/co/pragma/ms_bootcamp/main/MsBootcampApplication.java
@@ -0,0 +1,13 @@
+package co.pragma.ms_bootcamp.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBootcampApplication.class, args);
+	}
+
+}
---
+++ ms_bootcamp/src/test/java/co/pragma/ms_bootcamp/MsBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package co.pragma.ms_bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
